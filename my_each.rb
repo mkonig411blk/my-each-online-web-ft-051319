e498a36a0cb311002594a85d00f2e35d4e22a111
Margot Konig <margot.konig@blackrock.com>
2019-05-15 21:28:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(collection)
+  i = 0
+  array = []
+ while i < collection.length
+  array << yield(collection[i])
+     i += 1
+ end
+  collection
 end
